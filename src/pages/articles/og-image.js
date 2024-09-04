93208b8e7ee40aee136c4d538e82fe0373c4443d
leonidlewis <leonid.lewis@gmail.com>
2024-09-04 06:47:03 -0700

    fix the puppeteer

--- src/pages/articles/og-image.js
+++ src/pages/articles/og-image.js
@@ -1,20 +1,12 @@
-const path = require('path');
-const fs = require('fs');
-const { createHash } = require('crypto');
-const chromium = require('@sparticuz/chromium');
-const { executablePath } = require('puppeteer');
-// const puppeteer = require('puppeteer');
-const puppeteer = require("puppeteer-core");
-// const chromium = require('chrome-aws-lambda');
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
 export async function generateOgImage(props) {
   const params = new URLSearchParams(props);
   const url = `file:${path.join(
     process.cwd(),
-    // `src/pages/articles/og-image.html?${params}`
     `build/articles/og-image.html?${params}`
   )}`;
-  //  nconsole.log("rr ---"+url)
 
   const hash = createHash('md5').update(url).digest('hex');
   const ogImageDir = path.join(process.cwd(), `public/og`);
@@ -26,24 +18,21 @@ export async function generateOgImage(props) {
     fs.statSync(imagePath);
     return publicPath;
   } catch (error) {
-    // file does not exists, so we create it
+    // File does not exist, create it
   }
 
   const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    // args: chromium.args,
+    args: chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(),
+    executablePath: await chromium.executablePath,
     headless: chromium.headless,
   });
-  
+
   const page = await browser.newPage();
   await page.setViewport({ width: 1200, height: 630 });
   await page.goto(url, { waitUntil: 'networkidle0' });
   const buffer = await page.screenshot();
   await browser.close();
-
-  // res.status(200).json({ success: true });
 
   fs.mkdirSync(ogImageDir, { recursive: true });
   fs.writeFileSync(imagePath, buffer);
