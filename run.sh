00c16c8fc79827977dd75c5f96ddc6f26062b065
smartdev328 <davidlynn.tech@gmail.com>
2024-09-04 18:33:20 -0700

    update fox123

--- run.sh
+++
@@ -1,9 +0,0 @@
-docker build -t novqigarrix/nextjs-puppeteer .
-
-# Run in the container
-# docker container create --name nextjs-puppeteer -p 3000:3000 novqigarrix/nextjs-puppeteer
-
-# Expose to 3000
-# docker container create --name nextjs-puppeteer -p 3000:3000 novqigarrix/nextjs-puppeteer
-
-docker container start nextjs-puppeteer
